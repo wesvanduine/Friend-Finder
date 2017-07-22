36aa958a550529b6acec10d57a655bae178459ab
Wes Van Duine <wesvanduine@gmail.com>
2017-07-21 19:32:50 -0700

    home and survey bootstrap

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+
+// Dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
+
+// =============================================================
