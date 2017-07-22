e3f03a809dd98aa83fbca79295fe3445d2ced919
Wes Van Duine <wesvanduine@gmail.com>
2017-07-21 19:37:15 -0700

    setting up server.js dependancies

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-
 // Dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-
-const app = express();
-
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+// Seed data for "database"
+var friends = require('./app/data/friends.js');
 // =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000; // Sets an initial port.
+
+//makes static assets in the public folder available
+app.use(express.static('app/public'));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+
+
